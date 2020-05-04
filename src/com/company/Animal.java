6e1d16d165ddbc862b64a1e27d96ba3147a3b990
Darek <d.szkatula@gmail.com>
2020-05-04 21:12:48 +0200

    Exercise 0

---
+++ src/com/company/Animal.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.io.File;
+import java.sql.SQLOutput;
+
+public class Animal {
+    private Double weight;
+    public String name;
+    File pic;
+
+    }
+
+
+
+
